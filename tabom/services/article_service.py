6db78203db556f7df81850326258c7e8b5ef87c3
SW <aliwo@naver.com>
2022-01-06 21:02:12 +0900

    use capture queries

--- tabom/services/article_service.py
+++ tabom/services/article_service.py
@@ -1,6 +1,12 @@
+from django.db.models import QuerySet
+
 from tabom.models import Article
 
 
 def get_an_article(article_id: int) -> Article:
     article = Article.objects.filter(id=article_id).get()
     return article
+
+
+def get_article_list(offset: int, limit: int) -> QuerySet[Article]:
+    return Article.objects.order_by("-id")[offset : offset + limit]
