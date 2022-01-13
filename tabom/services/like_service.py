3c6730fa4ed33389cb1d63485e80f6c22e60f689
SW <aliwo@naver.com>
2022-01-13 18:48:05 +0900

    use F expression

--- tabom/services/like_service.py
+++ tabom/services/like_service.py
@@ -1,20 +1,18 @@
+from django.db.models import F
+
 from tabom.models import Article, Like, User
 
 
 def do_like(user_id: int, article_id: int) -> Like:
     User.objects.filter(id=user_id).get()
-    article = Article.objects.filter(id=article_id).get()
+    Article.objects.filter(id=article_id).get()
 
     like = Like.objects.create(user_id=user_id, article_id=article_id)
-    article.like_count += 1
-    article.save()
-
+    Article.objects.filter(id=article_id).update(like_count=F("like_count") + 1)
     return like
 
 
 def undo_like(user_id: int, article_id: int) -> None:
     deleted_cnt, _ = Like.objects.filter(user_id=user_id, article_id=article_id).delete()
     if deleted_cnt:
-        article = Article.objects.filter(id=article_id).get()
-        article -= 1
-        article.save()
+        Article.objects.filter(id=article_id).update(like_count=F("like_count") - 1)
