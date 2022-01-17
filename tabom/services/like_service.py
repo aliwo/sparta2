55b7ae75ba7f1fadd972a7aa713f60715c13c4d4
SW <aliwo@naver.com>
2022-01-17 20:46:12 +0900

    use transaction atomic

--- tabom/services/like_service.py
+++ tabom/services/like_service.py
@@ -1,8 +1,10 @@
+from django.db import transaction
 from django.db.models import F
 
 from tabom.models import Article, Like, User
 
 
+@transaction.atomic
 def do_like(user_id: int, article_id: int) -> Like:
     User.objects.filter(id=user_id).get()
     Article.objects.filter(id=article_id).get()
@@ -12,6 +14,7 @@ def do_like(user_id: int, article_id: int) -> Like:
     return like
 
 
+@transaction.atomic
 def undo_like(user_id: int, article_id: int) -> None:
     deleted_cnt, _ = Like.objects.filter(user_id=user_id, article_id=article_id).delete()
     if deleted_cnt:
