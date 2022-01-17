9998a5e51103844817b0eea79ad266d0155eae9e
SW <aliwo@naver.com>
2022-01-17 22:07:02 +0900

    all done; including async view

--- tabom/services/like_service.py
+++ tabom/services/like_service.py
@@ -1,3 +1,6 @@
+from typing import cast
+
+from asgiref.sync import sync_to_async
 from django.db import transaction
 from django.db.models import F
 
@@ -14,8 +17,17 @@ def do_like(user_id: int, article_id: int) -> Like:
     return like
 
 
+async def async_do_like(user_id: int, article_id: int) -> Like:
+    result = await sync_to_async(do_like)(user_id, article_id)
+    return cast(Like, result)
+
+
 @transaction.atomic
 def undo_like(user_id: int, article_id: int) -> None:
     deleted_cnt, _ = Like.objects.filter(user_id=user_id, article_id=article_id).delete()
     if deleted_cnt:
         Article.objects.filter(id=article_id).update(like_count=F("like_count") - 1)
+
+
+async def async_undo_like(user_id: int, article_id: int) -> None:
+    await sync_to_async(undo_like)(user_id, article_id)
