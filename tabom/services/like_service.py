46c36fc040b3a2b9dca70f5f7d82dada659af751
triumph1 <seungwon.jeong@deliveryhero.co.kr>
2022-02-04 14:09:47 +0900

    use select_for_update()

--- tabom/services/like_service.py
+++ tabom/services/like_service.py
@@ -10,7 +10,7 @@ from tabom.models import Article, Like, User
 @transaction.atomic
 def do_like(user_id: int, article_id: int) -> Like:
     User.objects.filter(id=user_id).get()
-    Article.objects.filter(id=article_id).get()
+    Article.objects.filter(id=article_id).select_for_update().get()
 
     like = Like.objects.create(user_id=user_id, article_id=article_id)
     Article.objects.filter(id=article_id).update(like_count=F("like_count") + 1)
