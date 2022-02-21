12afba289f65cf76c35c31524bd03ca287cd232a
triumph1 <seungwon.jeong@deliveryhero.co.kr>
2022-02-21 10:17:11 +0900

    update first, do not use select_for_update()

--- tabom/services/like_service.py
+++ tabom/services/like_service.py
@@ -10,10 +10,10 @@ from tabom.models import Article, Like, User
 @transaction.atomic
 def do_like(user_id: int, article_id: int) -> Like:
     User.objects.filter(id=user_id).get()
-    Article.objects.filter(id=article_id).select_for_update().get()
+    Article.objects.filter(id=article_id).get()
 
-    like = Like.objects.create(user_id=user_id, article_id=article_id)
     Article.objects.filter(id=article_id).update(like_count=F("like_count") + 1)
+    like = Like.objects.create(user_id=user_id, article_id=article_id)
     return like
 
 
